34769971ea646f70ec096895025b29d18dfe34d2
Arhan13 <arhanchoudhury@gmail.com>
2021-05-09 02:06:44 +0530

    NQueen

--- Backtracking/NQueen.cpp
+++ Backtracking/NQueen.cpp
@@ -1,44 +1,81 @@
 class Solution
 {
 public:
+    // vector<vector<string>> solveNQueens(int n) {
+    //     vector<vector<string>> res;
+    //     vector<string> nQueens(n, string(n, '.'));
+    //     solvenqueens(res, nQueens, 0, n);
+    //     return res;
+    // }
+
     vector<vector<string>> solveNQueens(int n)
     {
-        vector<vector<string>> res;
-        vector<string> nQueens(n, string(n, '.'));
-        solveNQueens(res, nQueens, 0, n);
-        return res;
+        //I need to give all the solutions
+        vector<vector<string>> response;
+        //Canvas for inidi=vidual queen setup
+        vector<string> nQueen(n, string(n, '.'));
+        solvenqueen(response, nQueen, 0, n);
+        return response;
     }
 
-private:
-    void solveNQueens(vector<vector<string>> &res, vector<string> &nQueens, int row, int &n)
+    void solvenqueen(vector<vector<string>> &res, vector<string> &nQueen, int row, int n)
     {
         if (row == n)
         {
-            res.push_back(nQueens);
+            res.push_back(nQueen);
             return;
         }
-        for (int col = 0; col != n; ++col)
-            if (isValid(nQueens, row, col, n))
+        for (int i = 0; i < n; i++)
+        {
+            if (isValid(nQueen, row, i, n))
             {
-                nQueens[row][col] = 'Q';
-                solveNQueens(res, nQueens, row + 1, n);
-                nQueens[row][col] = '.';
+                nQueen[row][i] = 'Q';
+                solvenqueen(res, nQueen, row + 1, n);
+                //Backtracking
+                nQueen[row][i] = '.';
             }
+        }
     }
-    bool isValid(vector<string> &nQueens, int row, int col, int &n)
+
+    // void solvenqueens(vector<vector<string>> &res, vector<string> &nQueens, int row, int n){
+    //     if(row==n){
+    //         res.push_back(nQueens);
+    //         return;
+    //     }
+    //     for(int col=0;col!=n;col++){
+    //         if(isValid(nQueens, row, col, n)){
+    //             nQueens[row][col] = 'Q';
+    //             solvenqueens(res, nQueens, row+1, n);
+    //             //Backtracking process
+    //             nQueens[row][col] = '.';
+    //         }
+    //     }
+    // }
+
+    bool isValid(vector<string> &nQueens, int row, int col, int n)
     {
-        //check if the column had a queen before.
-        for (int i = 0; i != row; ++i)
+        //checking if the column has a queen before this
+        for (int i = 0; i < n; i++)
+        {
             if (nQueens[i][col] == 'Q')
+            {
                 return false;
-        //check if the 45° diagonal had a queen before.
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j)
+            }
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+        {
             if (nQueens[i][j] == 'Q')
+            {
                 return false;
-        //check if the 135° diagonal had a queen before.
-        for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j)
+            }
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j <= n; i--, j++)
+        {
             if (nQueens[i][j] == 'Q')
+            {
                 return false;
+            }
+        }
         return true;
     }
 };
