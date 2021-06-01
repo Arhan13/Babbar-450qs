85ac63c9c53a73e932405657286544bc1c1bb734
Arhan13 <arhanchoudhury@gmail.com>
2021-06-01 23:17:39 +0530

    Added Changes

--- Binary Tree/HeightOfTree.cpp
+++ Binary Tree/HeightOfTree.cpp
@@ -1,18 +1,18 @@
 //User function template for C++
 
-/*
 struct Node
 {
     int data;
-    struct Node* left;
-    struct Node* right;
-    
-    Node(int x){
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
         data = x;
         left = right = NULL;
     }
 };
-*/
+
 class Solution
 {
 public:
